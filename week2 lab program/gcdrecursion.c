ff310aaab53e0219426772d870ebe50c57dda8da
Nishchal-Nandagopal <71306684+Nishchal-Nandagopal@users.noreply.github.com>
2021-04-27 15:35:56 +0530

    Add files via upload

---
+++ week2 lab program/gcdrecursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int gcd(int a, int b)
+{
+if (b == 0)
+return a;
+int temp=a%b;
+return gcd(b, temp);
+}
+
+int main()
+{
+	int a,b;
+	printf("Enter two non-zero numbers\n");
+    scanf("%d %d",&a,&b);
+	printf("Gcd of two numbers is %d\n",gcd(a,b));
+           
+}
